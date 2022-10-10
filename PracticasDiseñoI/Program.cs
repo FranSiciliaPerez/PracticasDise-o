5ef2b356028a3f4c9d7e35da56216c8a3620d4bd
Francisco Javier Sicilia Pérez <franjsp23@gmail.com>
2022-10-10 13:16:43 +0200

    Hecho

--- PracticasDiseñoI/Program.cs
+++ PracticasDiseñoI/Program.cs
@@ -10,6 +10,11 @@ namespace PracticasDiseñoI
     {
         static void Main(string[] args)
         {
+            //Área del círculo
+            Console.WriteLine("Por favor, añade el radio del circulo");
+            int radio_circulo = int.Parse(Console.ReadLine());
+            double area = Math.Pow(radio_circulo, 2) * Math.PI;
+            Console.WriteLine($"El área del circulo de radio {radio_circulo} es: {area}");
         }
     }
 }
